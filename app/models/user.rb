8088645c290fa637c6839763fff7a82c1a6ed76d
Marcel Benitez <marcelbm94@gmail.com>
2015-03-21 16:05:22 -0600

    @itesm mail faggots

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   # Validations
   # :email
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_format_of :email, with: /[Aa]\d+@itesm\.mx/
 
   def self.paged(page_number)
     order(admin: :desc, email: :asc).page page_number
