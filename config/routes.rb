94ec07575ee9a40eb15a6f51b71a2c80d136d009
Marcel Benitez <marcelbm94@gmail.com>
2015-03-22 07:21:13 -0600

    Cambio de gome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Kiin::Application.routes.draw do
-  root "pages#home"
-  get "home", to: "pages#home", as: "home"
+  root "pages#inside"
+  get "home", to: "pages#inside", as: "home"
   get "inside", to: "pages#inside", as: "inside"
   get "/contact", to: "pages#contact", as: "contact"
   post "/emailconfirmation", to: "pages#email", as: "email_confirmation"
